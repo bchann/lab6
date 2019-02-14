d2980db2c7fca149d0e33cbfb387bb4e3d0691d2
Brian Chan <alto1020@gmail.com>
2019-02-14 10:25:10 -0800

    lab6 done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,29 +2,54 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
-})
+  initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+  $('.project a').click(addProjectDetails);
 
-	// $('#colorBtn').click(randomizeColors);
+  // $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevent following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var projectID = $(this)
+    .closest('.project')
+    .attr('id');
+  // get rid of 'project' from the front of the id 'project3'
+  var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+  console.log('User clicked on project ' + idNumber);
+
+  var url = '/project/' + idNumber;
+  console.log('URL: ' + url);
+
+  $.get(url, callback);
+}
+
+function callback(res) {
+  console.log(res);
+
+  var projDetails =
+    '<p>' +
+    res.title +
+    '</p>' +
+    '<p>' +
+    res.date +
+    '</p>' +
+    '<img src="' +
+    res.image +
+    '" class="proj-img"></img>' +
+    res.summary;
+
+  $('.project#project' + res.id + ' .details').html(projDetails);
 }
